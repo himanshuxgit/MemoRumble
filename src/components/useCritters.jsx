d96b167b9c9373e8694897fb167cc9fd2549e67f
Himanshu Kashyap <38201652+himanshuxgit@users.noreply.github.com>
2023-08-28 00:09:05 +0530

    modified the hook

--- src/components/useCritters.jsx
+++ src/components/useCritters.jsx
@@ -1,4 +1,3 @@
-// components/useCritters.js
 import { useState } from "react";
 
 export default function useCritters() {
@@ -11,17 +10,23 @@ export default function useCritters() {
     return data;
   };
 
-  const getRandomCritters = async (amount) => {
+  const getRandomCritter = async () => {
+    const randomId = Math.floor(Math.random() * POSSIBLE_CRITTERS) + 1;
+    const url = `https://pokeapi.co/api/v2/pokemon/${randomId}`;
+    const critterData = await fetchPokemonData(url);
+
     const shinyProbability = 0.1; // 10% chance of a shiny critter
+    const isShiny = Math.random() < shinyProbability;
+
+    return { ...critterData, shiny: isShiny };
+  };
+
+  const getRandomCritters = async (amount) => {
     const crittersToShow = [];
 
     while (crittersToShow.length < amount) {
-      const randomId = Math.floor(Math.random() * POSSIBLE_CRITTERS) + 1;
-      const url = `https://pokeapi.co/api/v2/pokemon/${randomId}`;
-      const critterData = await fetchPokemonData(url);
-
-      const isShiny = Math.random() < shinyProbability;
-      crittersToShow.push({ ...critterData, shiny: isShiny });
+      const critter = await getRandomCritter();
+      crittersToShow.push(critter);
     }
 
     return crittersToShow;
@@ -31,7 +36,10 @@ export default function useCritters() {
     const shuffledArray = [...array];
     for (let i = shuffledArray.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
-      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
     }
     return shuffledArray;
   };
