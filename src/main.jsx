bf0d3d0df688ecb963a6f4923d34053d046ecdb6
Himanshu Kashyap <38201652+himanshuxgit@users.noreply.github.com>
2023-08-30 10:32:39 +0530

    switched to TailwindCSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import CritterGame from "./components/CritterGame";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
