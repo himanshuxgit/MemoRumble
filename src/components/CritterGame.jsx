21f5d5c36833b30bbab69419b71bab113e8cf40f
Himanshu Kashyap <38201652+himanshuxgit@users.noreply.github.com>
2023-09-16 16:05:29 +0530

    added background audio

--- src/components/CritterGame.jsx
+++ src/components/CritterGame.jsx
@@ -3,27 +3,43 @@ import useCritters from "./useCritters";
 import CritterCard from "./CritterCard";
 import "../styles/CritterGame.css";
 import logo from "/logo.png";
+import pokeAudio from "../assets/Pokemon.mp3";
+import playAudio from "./playAudio";
+import Switch from "react-switch";
+
+const bgAudio = new Audio(pokeAudio);
 
 function CritterGame() {
-  const { critters, getRandomCritters, shuffleCritters, setCritters } = useCritters();
+  playAudio(bgAudio);
+
+  const {
+    critters,
+    getRandomCritters,
+    shuffleCritters,
+    setCritters,
+  } = useCritters();
   const [selectedCritters, setSelectedCritters] = useState([]);
   const [isShuffling, setIsShuffling] = useState(false);
   const [score, setScore] = useState(0);
   const [isFirstSelection, setIsFirstSelection] = useState(true);
   const [bestScore, setBestScore] = useState(0);
   const [hasWon, setHasWon] = useState(false);
-  const [isLoading, setIsLoading] = useState(true); // Add loading state
-
+  const [isLoading, setIsLoading] = useState(true);
+  const [isMuted, setIsMuted] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
       const randomCritters = await getRandomCritters(10);
       setCritters(randomCritters);
-      setIsLoading(false); // Cards are loaded, set loading to false
-
+      setIsLoading(false);
     }
     fetchData();
   }, []);
+
+  const toggleMute = () => {
+    setIsMuted(!isMuted);
+    bgAudio.muted = !isMuted;
+  };
 
   const handleCritterClick = (critter) => {
     if (selectedCritters.some((selected) => selected.id === critter.id)) {
@@ -64,9 +80,8 @@ function CritterGame() {
   }, [score, bestScore, critters.length, hasWon]);
 
   const handleReloadClick = () => {
-    window.location.reload(); // Refresh the webpage
+    window.location.reload();
   };
-
 
   return (
     <div>
@@ -76,35 +91,46 @@ function CritterGame() {
         </div>
         <h1 className="game-name">MemoRumble</h1>
         <div className="score-bar">
-          <div className="score">Score: <span className="score-value">{score}</span></div>
-          <div className="best-score">Best Score: <span className="score-value">{bestScore}</span></div>
+          <div className="score">
+            Score: <span className="score-value">{score}</span>
+          </div>
+          <div className="best-score">
+            Best Score: <span className="score-value">{bestScore}</span>
+          </div>
         </div>
       </nav>
-      {isLoading ? ( // Show loading logo while cards are loading
+      {isLoading ? (
         <div className="loading-logo-container">
           <img src={logo} alt="Loading..." className="loading-logo" />
-          <div className="loadingMsg"><p>Loading...</p></div>
+          <div className="loadingMsg">
+            <p>Loading...</p>
+          </div>
         </div>
       ) : (
-      <div className={`critter-list ${isShuffling ? 'shuffling' : ''}`}>
-        {hasWon ? (
-          <div className="winning-message">
-            <p>You are the best! You won the Game!</p>
-            <button className="close-button" onClick={handleReloadClick}>
-              Close
-            </button>
-          </div>
-        ) : (
-          critters.map((critter) => (
-            <CritterCard
-              key={critter.id}
-              critter={critter}
-              onClick={() => handleCritterClick(critter)}
-            />
-          ))
-        )}
-      </div>
+        <div className={`critter-list ${isShuffling ? "shuffling" : ""}`}>
+          {hasWon ? (
+            <div className="winning-message">
+              <p>You are the best! </p>
+              <p>You Won!</p>
+              <button className="close-button" onClick={handleReloadClick}>
+                Play Again
+              </button>
+            </div>
+          ) : (
+            critters.map((critter) => (
+              <CritterCard
+                key={critter.id}
+                critter={critter}
+                onClick={() => handleCritterClick(critter)}
+              />
+            ))
+          )}
+        </div>
       )}
+      {/* Position the mute switch in the bottom right corner */}
+      <div className="mute-switch-container">
+        <Switch onChange={toggleMute} checked={!isMuted} className="react-switch" />
+      </div>
     </div>
   );
 }
